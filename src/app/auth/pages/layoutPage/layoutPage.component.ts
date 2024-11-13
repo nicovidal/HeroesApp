96304d4fdc280b117cc30fc633302d32e73ccee1
nicovidal <nico.vidal.m@gmail.com>
2024-11-13 17:10:43 -0300

    first commit

---
+++ src/app/auth/pages/layoutPage/layoutPage.component.ts
@@ -0,0 +1,8 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-page',
+  templateUrl:'layoutPage.component.html'
+})
+export class LayoutPageComponent { }
